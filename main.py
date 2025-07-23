3b04ecddce3522629def31ca2e3b4d9712263df4
Alexander Khun <alexanderkhun24@gmail.com>
2025-07-23 13:14:05 -0600

    Added correct letter/incorrect position checks.

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from word_list import dictionary
 num_attempts = 6
 
 word_choice = (random.choice(dictionary)).upper()
+
+#list to store letters in word for checking
+choice_letters = []
+for letter in word_choice:
+    if letter not in choice_letters:
+        choice_letters.append(letter)
+
 print(word_choice)
+print(choice_letters)
 
 #Initial guess before loop
 user_guess = input ("Guess the word: ")
@@ -14,8 +22,19 @@ if word_choice == user_guess:
     print("✅✅✅✅✅")
     exit(0)
 else:
-    for i in range(5):
-        if user_guess[i] == word_choice[i]:
-            print("✅", end="")
-        elif user_guess[i] != word_choice[i]:
-            print("⬛", end="")
+    while num_attempts > 0:
+        for i in range(5):
+            if user_guess[i] == word_choice[i]:
+                print("✅", end="")
+            elif user_guess[i] != word_choice[i] and user_guess[i] in choice_letters:
+                print("🟨", end="")
+
+            elif user_guess[i] != word_choice[i]:
+                print("⬛", end="")
+        num_attempts -= 1
+
+        user_guess = input("\nGuess the word: ")
+        user_guess = user_guess.upper()
+        if word_choice == user_guess:
+            print("✅✅✅✅✅")
+            exit(0)
